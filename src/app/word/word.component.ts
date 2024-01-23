0451baaf317c186fd471ff1231abfabacce980e9
LeoMafille <leomafille.pro@gmail.com>
2024-01-23 19:49:01 +0100

    Removed accents from words when checking if the word was found

--- src/app/word/word.component.ts
+++ src/app/word/word.component.ts
@@ -11,7 +11,7 @@ export class WordComponent {
   @Input('word') word!: String;
   
   tryWord(word:String):void {
-    if(this.hidden && this.word.toLocaleLowerCase() == word.toLocaleLowerCase()){
+    if(this.hidden && this.word.toLocaleLowerCase() == word.toLocaleLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")){
       this.hidden = false;
       this.justRevealed = true;
     } else {
