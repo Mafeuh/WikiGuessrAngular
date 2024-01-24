43632f7e9501d648b06d11b744cc227a1303ce55
LeoMafille <leomafille.pro@gmail.com>
2024-01-24 14:27:36 +0100

    Fixed API call

--- src/app/tools/wiki-loader.ts
+++ src/app/tools/wiki-loader.ts
@@ -1,8 +1,8 @@
 export class WikiLoader {
     get_page(){
         // Replace 'your_url_here' with the actual URL you want to fetch data from
-        //const apiUrl = 'http://mafeuhnas.ddns.net:8000/getpage';
-        const apiUrl = 'http://localhost:8000/getpage'
+        const apiUrl = 'http://mafeuhnas.ddns.net:8000/getpage';
+        //const apiUrl = 'http://localhost:8000/getpage'
         // Using the fetch API
         return fetch(apiUrl)
     }
